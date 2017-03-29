fede805ab51ae48889e11124760b4f953d35536e
xiejianan <xiejianan86@gmail.com>
2017-03-30 01:30:48 +0800

    all

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,32 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post_group = @group
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+  redirect_to account_posts_path, notice: "編輯成功"
+else
+  render :edit
+end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "删除文章成功"
+    redirect_to groups_path
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
