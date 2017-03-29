8e1b97e96da3eb33cecbbcf92b503ce084269f6e
xiejianan <xiejianan86@gmail.com>
2017-03-30 00:10:01 +0800

    all

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,7 @@ before_action :find_group_and_check_permission,only:[:edit,:update,:destroy]
   def show
     @group = Group.find(params[:id])
     @posts = @group.posts
-    @posts = @group.posts.recent
+    @posts = @group.posts.recent.paginate(:page => params[:page],:per_page => 3)
   end
 
   def new
